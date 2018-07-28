3e98bc3e5da2adcf4bd1bb1ce7079dd7a48c404b
Brooke Rodenhizer <brooketaylorrodenhizer@gmail.com>
2018-07-28 16:23:16 -0400

    Rails: Validations Checkpoint

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,9 +4,15 @@ RSpec.describe Topic, type: :model do
   let(:name) { RandomData.random_sentence }
   let(:description) { RandomData.random_paragraph }
   let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-  it { is_expected.to have_many(:posts) }
+
+  it { is_expected.to have_many(:posts)
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
 
 # #1
   describe "attributes" do
