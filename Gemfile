9d9c3440b32667438db6950578864c965bb99698
Brooke Rodenhizer <brooketaylorrodenhizer@gmail.com>
2018-07-29 13:51:39 -0400

    RSpec errors for Rails: Authentication and User Model checkpoint

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'bootstrap-sass'
 # Use bootstrap CSS framework
+# Use for encrypting passwords
+gem 'bcrypt'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_184731) do
+ActiveRecord::Schema.define(version: 2018_07_29_163855) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -69,6 +69,14 @@ ActiveRecord::Schema.define(version: 2018_07_28_184731) do
     t.string "name"
     t.boolean "public", default: true
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
